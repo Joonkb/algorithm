66b4af43c78f3a7aad10eb68f319d007a1f7b8af
Joonkb <byeongjunkim@naver.com>
2022-01-21 01:49:31 +0900

    큰 수 문제풀이(C++) 주석 추가

--- Programmers/Lv2/making-a-big-number.cpp
+++ Programmers/Lv2/making-a-big-number.cpp
@@ -1,3 +1,19 @@
+/**
+ * @author: ByeongJoon Kim
+ * @Judge_site: programmers
+ * @level: 2
+ * @prob_name: 큰 수 만들기
+ * @prob_url: https://programmers.co.kr/learn/courses/30/lessons/42883
+ *
+ */
+
+ /*
+ * @desc : 브루트 포스 문제로 착각 ( 모든 경우의 수를 만들어 보자) 
+           -> 1,000,000 C 999,999 가지의 경우의수
+   10^6 : 이라는 점에서 nlogn, n 의 시간복잡도인 풀이를 생각해야함
+ 
+ */
+
 #include <string>
 #include <vector>
 
