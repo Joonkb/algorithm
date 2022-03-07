21cce247382ac464f72c84b482130ca4393054fd
Joonkb <byeongjunkim@naver.com>
2022-03-07 23:50:00 +0900

    백준 구현문제 풀이 (JAVA)

--- BOJ/구현/기초/p1913.java
+++ BOJ/구현/기초/p1913.java
@@ -1,42 +1,54 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class p1913 {
 
-    static int[] dx = {0, 1,  0,  -1};
-    static int[] dy = {1, 0, -1,   0};
+    static int N, what;
+    static int dx[] = {1, 0, -1, 0};
+    static int dy[] = {0, 1, 0, -1};
 
-    public static void main(String[] args) {
+    public static boolean isInRange(int x, int y) {
+        if (0 > x || x >= N || 0 > y || y >= N) return false;
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int findNum = sc.nextInt();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = sc.nextInt();
+        what = sc.nextInt();
+        int[][] board = new int[N][N];
 
-        int arr[][] = new int[N][N];
-        int cx = (N - 1) / 2,  ax = 0;
-        int cy = (N - 1) / 2,  ay = 0;
-        int num = 1;
-        arr[cx][cy] = num;
-        cx--;
-
-        for (int cnt = 1; cnt <= N / 2; ++cnt) {
-            for (int dir = 0; dir < 4; ++dir) {
-                for (int w = 0; w < cnt * 2; w++) {
-                    System.out.println("cx,  cy = " + cx+ " " + cy);
-                    arr[cx][cy] = ++num;
-                    if (num == findNum) {
-                        ax = cx + 1;
-                        ay = cy + 1;
-                    }
-                    cx += dx[dir];
-                    cy += dy[dir];
+        int x = 0, y = 0, num = N * N, dir = 0;
+        int cx = 0, cy = 0;
+        while (isInRange(x, y) && board[x][y] == 0) {
+            board[x][y] = num--;
+            int nx = x + dx[dir], ny = y + dy[dir];
+            while (isInRange(nx, ny) && board[nx][ny] == 0) {
+                x = nx;
+                y = ny;
+                board[nx][ny] = num--;
+                nx += dx[dir];
+                ny += dy[dir];
+            }
+            dir = (dir + 1) % 4;
+            x += dx[dir];
+            y += dy[dir];
+        }
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                bw.write(board[i][j] +" ");
+                if(board[i][j] == what){
+                    cx = i + 1;
+                    cy = j + 1;
                 }
             }
+            bw.write("\n");
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.println(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println(ax + " " + ay);
+        bw.write(cx + " " + cy + "\n");
+        bw.flush();
     }
 }
