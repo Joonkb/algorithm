6b805ab125e8d27fd958c38c1a8d074f8c981d21
Joonkb <byeongjunkim@naver.com>
2023-09-10 18:47:15 +0900

    이상한문자만들기 해결 및 리팩토링

--- Programmers/Lv1/이상한문자만들기.java
+++ Programmers/Lv1/이상한문자만들기.java
@@ -2,15 +2,23 @@ package W11;
 
 public class 이상한문자만들기 {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (String word : s.split(" ")) {
-            for (int idx = 0; idx < word.length(); ++idx) {
-                if(word.charAt(idx) == ' ') sb.append(" ");
-                if (idx % 2 == 0) sb.append((word.charAt(idx)+"").toUpperCase());
-                else sb.append((word.charAt(idx)+"").toLowerCase());
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+        for (Character ch : s.toCharArray()) {
+            if(ch ==  ' ') {
+                idx = 0;
+                answer.append(' ');
+                continue;
             }
-            sb.append(" ");
+            answer.append(processIfNotSpace(ch, idx++));
         }
-        return sb.toString().trim();
+        return answer.toString();
+    }
+
+    private String processIfNotSpace(Character ch, int idx) {
+        if (idx % 2 == 0) {
+            return (ch + "").toUpperCase();
+        }
+        return (ch + "").toLowerCase();
     }
 }
